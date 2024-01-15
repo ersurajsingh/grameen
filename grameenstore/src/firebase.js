e109d49f469c1b9c4967cab3aaa8c1a67dd3f4dd
Suraj Singh <emailtosuraj.007@gmail.com>
2024-01-16 00:19:39 +0530

    firebase.js file created and setup

---
+++ grameenstore/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAFcumMCOUaqyBj_tim8YfGT-tF14ZFFrE",
+    authDomain: "grameen-store.firebaseapp.com",
+    projectId: "grameen-store",
+    storageBucket: "grameen-store.appspot.com",
+    messagingSenderId: "177613864881",
+    appId: "1:177613864881:web:40454a0ed4b02cae8d4790",
+    measurementId: "G-6L5N40HBP8"
+  };
